71b46cd5adf91b8c222ca8ee03dbedf42b0f099d
sgooch <stephanie.gooch@kpmg.co.uk>
2020-07-28 17:11:40 +0100

    rm backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket   = "cloudops-newsandbox.tfstate-bucket"
-    key      = "resize/terraform.tfstate"
-    region   = "eu-west-1"
-    role_arn = "arn:aws:iam::324382802360:role/SSOAdmin"
-    encrypt  = "true"
-  }
-}
